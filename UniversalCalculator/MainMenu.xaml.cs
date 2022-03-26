4a92e436c4a22e8478147935dd952f558808ae2b
001164255 <yuanyuan.cheng@student.tafesa.edu.au>
2022-03-26 17:29:12 +1030

    Implement MortgageCalculator navigation

--- UniversalCalculator/MainMenu.xaml.cs
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -26,5 +26,10 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void MortgageButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MortgageCalculator));
+		}
 	}
 }
